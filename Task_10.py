4c612ab91825c19ffd1ad4cb18ba6fd1542f4dd1
MageSim1377 <maxizobretatel@gmail.com>
2025-09-23 00:07:48 +0300

    t10

---
+++ Task_10.py
@@ -0,0 +1,9 @@
+a = int(input("Enter number a: "))
+b = int(input("Enter number b: "))
+
+print("a + b = " + str(a+b))
+print("a - b = " + str(a-b))
+print("a * b = " + str(a*b))
+print("a // b = " + str(a//b))
+print("a mod b = " + str(a%b))
+print("a ^ b = " + str(a**b))
