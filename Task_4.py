30bdf78b4e73787c534c86b5eaa3ad4016ede160
MageSim1377 <maxizobretatel@gmail.com>
2025-10-01 22:54:31 +0300

    add task 4

---
+++ Task_4.py
@@ -0,0 +1,27 @@
+numberStr1 = input("Enter first list of nunmbers: ")
+numberStr2 = input("Enter second list of nunmbers: ")
+
+numSet1 = numberStr1.split()
+numSet2 = numberStr2.split()
+
+intersection = []
+firstWithoutSecond = []
+secondWithoutFirst = []
+unionWithoutIntersection = []
+
+for i in numSet1:
+    if i in numSet2:
+        intersection.append(i)
+    if not i in numSet2:
+        firstWithoutSecond.append(i)
+
+for i in numSet2:
+    if not i in numSet1:
+        secondWithoutFirst.append(i)
+
+unionWithoutIntersection = firstWithoutSecond + secondWithoutFirst
+
+print(f"Numbers in both lists: {intersection}")
+print(f"Numbers in first but not in second list: {firstWithoutSecond}")
+print(f"Numbers in second but not in first list: {secondWithoutFirst}")
+print(f"Numbers that only present in one of lists: {unionWithoutIntersection}")
