74dff61b7556f3cbfac8e034b4b8cc1c20181815
MageSim1377 <136988092+MageSim1377@users.noreply.github.com>
2025-10-03 18:16:26 +0300

    Update Task_8.py

--- Task_8.py
+++ Task_8.py
@@ -1,19 +1,6 @@
 string = input("Enter string: ")
 
-startIter = 0
-endIter = len(string) - 1
-
-flag = True
-
-while startIter < endIter:
-    if string[startIter] != string[endIter]:
-        flag = False
-        break
-    else:
-        startIter += 1
-        endIter -= 1
-
-if flag:
+if string == string[::-1]:
     print("It is palindrom")
 else:
     print("It is not palindrom")
