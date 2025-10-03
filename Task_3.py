21d58aca1cf748ac89d8179cd1732b7c6129bc97
MageSim1377 <136988092+MageSim1377@users.noreply.github.com>
2025-10-03 18:13:41 +0300

    Update Task_3.py

--- Task_3.py
+++ Task_3.py
@@ -1,24 +1,8 @@
 password = input("Enter password: ")
 
-def checkIsOnlyNumbers(str):
-    flag = True
-    for i in str:
-        if ord(i) < ord('0') or ord(i) > ord('9'):
-            flag = False
-            break
-    return flag
-
-def checkIsOnlyLetters(str):
-    flag = True
-    for i in str:
-        if ord(i) < ord('A') or ord(i) > ord('z'):
-            flag = False
-            break
-    return flag
-
 if len(password) < 16:
     print("Too short")
-elif checkIsOnlyNumbers(password) or checkIsOnlyLetters(password):
+elif password.isalpha() or password.isdigit():
     print("Weak password")
 else:
-    print("Strong password")
+    print("Strong password! You're good, man! It is really good password!")
