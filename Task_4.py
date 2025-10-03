fd3242d6fcaaedd9067bb5993a63e93318c5d810
MageSim1377 <136988092+MageSim1377@users.noreply.github.com>
2025-10-03 18:39:28 +0300

    Update Task_4.py

--- Task_4.py
+++ Task_4.py
@@ -1,20 +1,20 @@
 money = int(input("Enter amount of money: "))
 
 if money // 100 > 0:
-    print(str(money // 100) + " banknotes of 100 rubles") 
+    print(f"{money // 100} banknotes of 100 rubles") 
     money %= 100
 if money // 50 > 0:
-    print(str(money // 50) + " banknotes of 50 rubles") 
+    print(f"{money // 50} banknotes of 50 rubles") 
     money %= 50
 if money // 10 > 0:
-    print(str(money // 10) + " banknotes of 10 rubles") 
+    print(f"{money // 10}  banknotes of 10 rubles") 
     money %= 10
 if money // 5 > 0:
-    print(str(money // 5) + " banknotes of 5 rubles") 
+    print(f"{money // 5}  banknotes of 5 rubles") 
     money %= 5
 if money // 2 > 0:
-    print(str(money // 2) + " coins of 2 rubles") 
+    print(f"{money // 2}  coins of 2 rubles") 
     money %= 2
 if money // 1 > 0:
-    print(str(money // 1) + " coins of 1 rubles") 
+    print(f"{money // 1}  coins of 1 rubles") 
     money %= 1
