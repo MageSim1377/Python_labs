1add486a6518890d820dd74928a6ee7731f54765
MageSim1377 <maxizobretatel@gmail.com>
2025-10-26 19:09:34 +0300

    upd task

--- Task.py
+++ Task.py
@@ -3,41 +3,77 @@ import random
 class StealingError(Exception):
     pass
 
+class IncorrectCustomerError(Exception):
+    pass
+
+class IncorrectAccountError(Exception):
+    pass
+
+
 class Bank:
+    customers = {}
     pass
 
 class Customer: 
-    accounts = {}
-
     def __init__(self):
+        self.accounts = {}
         self.id = random.randint(1, 100000)
+        while self.id in Bank.customers.keys():
+            self.id = random.randint(1, 100000)
+        Bank.customers[self.id] = self
     
     def openAccount(self, currency):
-        if currency in self.accounts.keys():
-            raise ValueError("No-no-no, mister Custumer, you only can have one account in each currency")
-        newAcc = Account(currency, cust)
-        self.accounts[currency] = newAcc
+        try:
+            if currency in self.accounts.keys():
+                raise ValueError()
+            newAcc = Account(currency, self)
+            self.accounts[currency] = newAcc
+        except ValueError:
+            print("No-no-no, mister Custumer, you can only have one account in each currency")
 
     def closeAccount(self, currency):
-        if currency not in self.accounts.keys():
-            raise ValueError("No-no-no, mister Custumer, you have not any account in this currency")
-        del self.accounts[currency]
+        try:
+            if currency not in self.accounts.keys():
+                raise ValueError()
+            del self.accounts[currency]
+        except ValueError:
+            print("No-no-no, mister Custumer, you don't have any account in this currency")
 
     def income(self, amount, currency):
         try:
             self.accounts[currency].balance += amount
         except (KeyError):
-            print("Man, you dont have account in this currency!")
+            print("Man, you don't have account in this currency!")
 
     def outcome(self, amount, currency):
         try:
             if(self.accounts[currency].balance >= amount):
                 self.accounts[currency].balance -= amount
+                print(f"Take your {amount} {currency}, man")
             else:
-                raise StealingError("Man, you have enough money, are you trying to rob us?")
+                raise StealingError("You don't have enough money!")
         except StealingError:
-            print("I am disappointed")
+            print("Man, you don't have enough money, are you trying to rob us? I am disappointed")
 
+    def transfer(self, amount, currency, reciever):
+        try:
+            if reciever not in Bank.customers.keys():
+                raise IncorrectCustomerError()
+            if currency not in Bank.customers[reciever].accounts.keys():
+                raise IncorrectAccountError()
+            if(self.accounts[currency].balance >= amount):
+                self.accounts[currency].balance -= amount
+                Bank.customers[reciever].accounts[currency].balance += amount
+                print("Transaction approved!")
+            else:
+                raise StealingError()
+        except IncorrectCustomerError:
+            print(f"Customer with id {reciever} doesn't exist")
+        except IncorrectAccountError:
+            print(f"Customer with id {reciever} doesn't have account in {currency}")
+        except StealingError:
+            print("You don't have enough money!")
+        pass
     
 
 
@@ -46,10 +82,12 @@ class Account:
         self.balance = 0
         self.currency = cur
         self.customer = cust
-        self
         pass
     pass
 
-cust = Customer()
-cust.openAccount("RAF")
-print(cust.accounts["RAF"].balance)
+cust1 = Customer()
+cust2 = Customer()
+cust1.openAccount("RAF")
+cust2.openAccount("RAF")
+cust1.income(500, "RAF")
+cust1.transfer(200, "RAF", cust2.id)
