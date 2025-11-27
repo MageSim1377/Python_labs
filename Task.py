a43a8b20909b80a4f8a650fcc529a9061254a729
MageSim1377 <maxizobretatel@gmail.com>
2025-11-28 00:02:39 +0300

    Task completed successfully and in time

--- Task.py
+++ Task.py
@@ -54,6 +54,8 @@ class Customer:
                 raise StealingError("You don't have enough money!")
         except StealingError:
             print("Man, you don't have enough money, are you trying to rob us? I am disappointed")
+        except (KeyError):
+            print("Man, you don't have account in this currency!")
 
     def transfer(self, amount, currency, reciever):
         try:
@@ -85,12 +87,74 @@ class Account:
         pass
     pass
 
-cust1 = Customer()
-cust2 = Customer()
-cust1.openAccount("RAF")
-cust2.openAccount("RAF")
-cust1.income(500, "RAF")
-cust1.transfer(200, "RAF", cust2.id)
+cust = None
+while True:
+    print("1. Create customer\n2. Choose customer\n3. Print costomers\n")
+    choose = int(input("Your choise: "))
+    if choose == 1:
+        cust = Customer()
+        break
+    elif choose == 2:
+        try:
+            cust = Bank.customers[int(input("Enter ID: "))]
+            break
+        except KeyError:
+            print("There is no customer with that ID")
+    elif choose == 3:
+        for id in Bank.customers.keys():
+            print(id)
 
 while True:
-    print()
+    print("1. Open account\n2. Close account\n3. Deposit to account\n4. Withdraw from an account\n5. Transfer money\n" \
+    "6. Print costomers\n7. Change customer\n8. New customer\n9. Report\n10. Report into file\n11. Exit\n")
+    choose = input("Your choise: ")
+    if choose == '1':
+        currency = input("Enter currency: ")
+        cust.openAccount(currency)
+        pass
+    elif choose == '2':
+        currency = input("Enter currency: ")
+        cust.openAccount(currency)
+        pass
+    elif choose == '3':
+        currency = input("Enter currency: ")
+        amount = int(input("Enter amount: "))
+        cust.income(amount, currency)
+        pass
+    elif choose == '4':
+        currency = input("Enter currency: ")
+        amount = int(input("Enter amount: "))
+        cust.outcome(amount, currency)
+        pass
+    elif choose == '5':
+        currency = input("Enter currency: ")
+        amount = int(input("Enter amount: "))
+        cust2 = int(input("Enter ID you want to transfer to customer with: "))
+        cust.transfer(amount, currency, cust2)
+        pass
+    elif choose == '6':
+        for id in Bank.customers.keys():
+            print(id)
+        pass
+    elif choose == '7':
+        try:
+            cust = Bank.customers[int(input("Enter ID: "))]
+        except KeyError:
+            print("There is no customer with that ID")
+        pass
+    elif choose == '8':
+        cust = Customer()
+        pass
+    elif choose == '9':
+        for cur, acc in cust.accounts.items():
+            print(f"{cur}: {acc.balance}")
+        print()1
+        
+    elif choose == '10':
+        with open(f"{cust.id}.txt", "w") as f:
+            for cur, acc in cust.accounts.items():
+                f.write(f"{cur}: {acc.balance}")
+    elif choose == '11':
+        break
+    else:
+        print("Incorrect choise")
