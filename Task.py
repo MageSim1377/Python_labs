ed7aba3d245658f97d01d16413cbea88c9bffb13
MageSim1377 <maxizobretatel@gmail.com>
2025-12-25 16:41:24 +0300

    some changes

--- Task.py
+++ Task.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import seaborn as sns
 import pandas as pd
 import numpy as np
 
@@ -9,7 +10,7 @@ df = pd.read_excel('lab_4_part_5.xlsx', header=1)
 
 print(df.groupby('точка').size().reset_index(name='count'))
 
-fig, axes = plt.subplots(1, 4, figsize=(10, 7))
+fig, axes = plt.subplots(2, 4, figsize=(10, 7))
 
 data = df[['точка', 'товар', 'Продажи']]
 #data['place and product'] = df['точка'] + ' ' + df['товар']
@@ -17,7 +18,7 @@ data = df[['точка', 'товар', 'Продажи']]
 
 ax = np.ravel(axes)
 places = data['точка'].unique()
-for i in range(8):
+for i in range(7):
     axData = data[data['точка'] == places[i]].groupby('товар').sum().reset_index()[:20]
     #print(axData)
     ax[i].bar(axData['товар'], axData['Продажи'])
@@ -25,6 +26,11 @@ for i in range(8):
     ax[i].set_title(f"Place {i + 1}", loc='center')
 
 
+
 #axes[0].bar()
+
+df['month'] = pd.to_datetime(df['Дата']).dt.month
+df['year'] = pd.to_datetime(df['Дата']).dt.year
+sns.lmplot(data=df, x='month', y='Продажи')
 
 plt.show()
