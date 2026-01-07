919958c39c0a98eefaefead5ce8590205e14555a
MageSim1377 <maxizobretatel@gmail.com>
2026-01-07 18:49:28 +0300

    Christmas commit with updates

--- Task.py
+++ Task.py
@@ -6,10 +6,11 @@ import numpy as np
 
 df = pd.read_excel('lab_4_part_5.xlsx', header=1)
 
-#print(df.head())
+print(df.head())
 
 print(df.groupby('точка').size().reset_index(name='count'))
 
+"""
 fig, axes = plt.subplots(2, 4, figsize=(10, 7))
 
 data = df[['точка', 'товар', 'Продажи']]
@@ -17,6 +18,8 @@ data = df[['точка', 'товар', 'Продажи']]
 #print(data)
 
 ax = np.ravel(axes)
+df['month'] = pd.to_datetime(df['Дата']).dt.month
+df['year'] = pd.to_datetime(df['Дата']).dt.year
 places = data['точка'].unique()
 for i in range(7):
     axData = data[data['точка'] == places[i]].groupby('товар').sum().reset_index()[:20]
@@ -24,13 +27,36 @@ for i in range(7):
     ax[i].bar(axData['товар'], axData['Продажи'])
     ax[i].set_xticklabels(axData['товар'], rotation=90, ha='center', fontsize=5)
     ax[i].set_title(f"Place {i + 1}", loc='center')
-
+    
 
 
 #axes[0].bar()
+sns.lmplot(data=df[['точка', 'Продажи', 'month']].groupby('month').agg({
+        'Продажи': 'sum',
+        'точка': 'last'
+    }).reset_index(), x='month', y='Продажи')
 
-df['month'] = pd.to_datetime(df['Дата']).dt.month
-df['year'] = pd.to_datetime(df['Дата']).dt.year
-sns.lmplot(data=df, x='month', y='Продажи')
+plt.show()
+"""
 
+
+"""
+sales = df.groupby('товар')['Продажи'].sum().sort_values(ascending=False)
+
+plt.figure(figsize=(8, 8))
+plt.pie(sales.values[:10], 
+        labels=sales.index[:10], 
+        autopct='%1.1f%%')
+plt.title('10 most popular products')
+
+plt.show()
+"""
+
+df['month'] = df['Год-мес'] % 100
+
+plt.figure(figsize=(7, 4))
+data = df[['month', 'Продажи']].groupby('month').sum().reset_index()
+plt.plot(data['month'], data['Продажи'])
+plt.xlabel("Month")
+plt.ylabel("Sales")
 plt.show()
